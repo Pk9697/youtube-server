e3cc3802b4819fea4454dc813913a410bbe5fd5e
Prashant Kumar <prashant.aman9@gmail.com>
2024-03-18 20:59:26 +0530

    Add getUserTweets endpoint

--- src/controllers/api/v1/tweet.controller.js
+++ src/controllers/api/v1/tweet.controller.js
@@ -1,9 +1,10 @@
+import mongoose from 'mongoose'
 import { Tweet } from '../../../models/tweet.model.js'
+import { User } from '../../../models/user.model.js'
 import { ApiError } from '../../../utils/ApiError.js'
 import { ApiResponse } from '../../../utils/ApiResponse.js'
 import { asyncHandler } from '../../../utils/asyncHandler.js'
 
-// TODO: create tweet
 const createTweet = asyncHandler(async (req, res) => {
   const { content } = req.body
 
@@ -19,4 +20,75 @@ const createTweet = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, tweet, 'Tweet created successfully!'))
 })
 
-export { createTweet }
+const getUserTweets = asyncHandler(async (req, res) => {
+  const { userId } = req.params
+
+  const existingUser = await User.findById(userId)
+  if (!existingUser) {
+    throw new ApiError(404, 'User does not exist')
+  }
+
+  const tweets = await Tweet.aggregate([
+    {
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'owner',
+        foreignField: '_id',
+        as: 'owner',
+        pipeline: [
+          {
+            $project: {
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: 'likes',
+        localField: '_id',
+        foreignField: 'tweet',
+        as: 'likes',
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: '$likes',
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, '$likes.owner'] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $project: {
+        owner: 1,
+        content: 1,
+        likesCount: 1,
+        isLiked: 1,
+        createdAt: 1,
+      },
+    },
+  ])
+
+  return res.status(200).json(new ApiResponse(200, tweets, 'User Tweets fetched successfully!'))
+})
+
+export { createTweet, getUserTweets }
--- src/routes/api/v1/tweet.route.js
+++ src/routes/api/v1/tweet.route.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import { verifyJwt } from '../../../middlewares/auth.middleware.js'
-import { createTweet } from '../../../controllers/api/v1/tweet.controller.js'
+import { createTweet, getUserTweets } from '../../../controllers/api/v1/tweet.controller.js'
 
 const router = Router()
 
 router.use(verifyJwt)
 
 router.route('/create').post(createTweet)
+
+router.route('/users/:userId').get(getUserTweets)
 
 export default router
