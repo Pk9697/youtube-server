45cd11c99e0c0479cf9ce7c938d3c4d1d537f666
Prashant Kumar <prashant.aman9@gmail.com>
2024-03-18 17:00:15 +0530

    Add getPlaylistById and addVideoToPlaylist endpoints

--- src/controllers/api/v1/playlist.controller.js
+++ src/controllers/api/v1/playlist.controller.js
@@ -4,6 +4,7 @@ import { User } from '../../../models/user.model.js'
 import { ApiError } from '../../../utils/ApiError.js'
 import { ApiResponse } from '../../../utils/ApiResponse.js'
 import { asyncHandler } from '../../../utils/asyncHandler.js'
+import { Video } from '../../../models/video.model.js'
 
 const createPlaylist = asyncHandler(async (req, res) => {
   const { name, description } = req.body
@@ -20,8 +21,6 @@ const createPlaylist = asyncHandler(async (req, res) => {
 
   return res.status(201).json(new ApiResponse(201, playlist, 'Playlist created successfully!'))
 })
-
-// TODO: get user playlists
 
 const getUserPlaylists = asyncHandler(async (req, res) => {
   const { userId } = req.params
@@ -50,4 +49,133 @@ const getUserPlaylists = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, playlists, 'User Playlists fetched successfully'))
 })
 
-export { createPlaylist, getUserPlaylists }
+const getPlaylistById = asyncHandler(async (req, res) => {
+  const { playlistId } = req.params
+
+  const existingPlaylist = await Playlist.findById(playlistId)
+  if (!existingPlaylist) {
+    throw new ApiError(404, 'Playlist does not exist!')
+  }
+
+  const playlist = await Playlist.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(playlistId),
+      },
+    },
+    {
+      $lookup: {
+        from: 'videos',
+        localField: 'videos',
+        foreignField: '_id',
+        as: 'videos',
+        pipeline: [
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'owner',
+              foreignField: '_id',
+              as: 'owner',
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              owner: {
+                $arrayElemAt: ['$owner', 0],
+              },
+            },
+          },
+          {
+            $project: {
+              thumbnail: 1,
+              owner: 1,
+              title: 1,
+              views: 1,
+              createdAt: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'owner',
+        foreignField: '_id',
+        as: 'owner',
+        pipeline: [
+          {
+            $lookup: {
+              from: 'subscriptions',
+              localField: '_id',
+              foreignField: 'channel',
+              as: 'subscribers',
+            },
+          },
+          {
+            $addFields: {
+              subscribersCount: {
+                $size: '$subscribers',
+              },
+            },
+          },
+          {
+            $project: {
+              fullName: 1,
+              avatar: 1,
+              subscribersCount: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $arrayElemAt: ['$owner', 0],
+        },
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        description: 1,
+        videos: 1,
+        owner: 1,
+        createdAt: 1,
+      },
+    },
+  ])
+
+  return res.status(200).json(new ApiResponse(200, playlist, 'Playlist fetched successfully!'))
+})
+
+const addVideoToPlaylist = asyncHandler(async (req, res) => {
+  const { playlistId, videoId } = req.query
+
+  const existingPlaylist = await Playlist.findById(playlistId)
+  if (!existingPlaylist) {
+    throw new ApiError(404, 'Playlist does not exist!')
+  }
+
+  const existingVideo = await Video.findById(videoId)
+  if (!existingVideo) {
+    throw new ApiError(404, 'Video does not exist!')
+  }
+
+  const filteredPlaylistVideos = await existingPlaylist.videos.filter((id) => !id.equals(videoId))
+
+  existingPlaylist.videos = [videoId, ...filteredPlaylistVideos]
+  await existingPlaylist.save({ validateBeforeSave: false })
+
+  return res.status(200).json(new ApiResponse(200, existingPlaylist, 'Added video to playlist successfully!'))
+})
+
+export { createPlaylist, getUserPlaylists, getPlaylistById, addVideoToPlaylist }
--- src/routes/api/v1/playlist.route.js
+++ src/routes/api/v1/playlist.route.js
@@ -1,6 +1,11 @@
 import { Router } from 'express'
 import { verifyJwt } from '../../../middlewares/auth.middleware.js'
-import { createPlaylist, getUserPlaylists } from '../../../controllers/api/v1/playlist.controller.js'
+import {
+  addVideoToPlaylist,
+  createPlaylist,
+  getPlaylistById,
+  getUserPlaylists,
+} from '../../../controllers/api/v1/playlist.controller.js'
 
 const router = Router()
 
@@ -9,5 +14,9 @@ router.use(verifyJwt)
 router.route('/create').post(createPlaylist)
 
 router.route('/user/:userId').get(getUserPlaylists)
+
+router.route('/:playlistId').get(getPlaylistById)
+
+router.route('/add').patch(addVideoToPlaylist)
 
 export default router
